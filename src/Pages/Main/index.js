780bcdb6cdf91431ebe4a292ac99bc4159d2d23d
jorgepeniaranda <jorge.maxii25@gmail.com>
2023-08-11 13:23:22 -0300

    /main

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -5,8 +5,8 @@ import ButtonsBottomNav from './Components/ButtonsBottomNav'
 export default function Main(props) {
     return (
         <Container className='bg-white dark:bg-[#3d3e3e] flex items-center justify-center dark:text-white transition-all ease-linear'>
-            <p className='text-6xl font-semibold'>Bienvenido, {props.user}</p>
-            <nav className='dark:bg-[#222323] flex fixed w-full h-1/5 bottom-0 items-center shadow-[0_-10px_15px_-3px_rgb(0_0_0_/_0.1),_0_-4px_6px_-4px_rgb(0_0_0_/_0.1)] justify-around'>
+            <p className='text-6xl font-semibold mb-16'>Bienvenido, {props.user}</p>
+            <nav className='dark:bg-[#222323] flex fixed w-full h-36 bottom-0 select-none items-center shadow-[0_-10px_15px_-3px_rgb(0_0_0_/_0.1),_0_-4px_6px_-4px_rgb(0_0_0_/_0.1)] justify-around'>
                 <ButtonsBottomNav to='/manage'>Gestionar Contraseñas</ButtonsBottomNav>
                 <ButtonsBottomNav to='/profile'>Perfil</ButtonsBottomNav>
             </nav>
