fff320bbcbc6af7b2c371e49d56b0845cdb89b8a
jorgepeniaranda <jorge.maxii25@gmail.com>
2023-08-16 13:29:43 -0300

    fix textarea in /profile/personalization

--- src/Pages/Profile/Components/LabelInputProfile/index.js
+++ src/Pages/Profile/Components/LabelInputProfile/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+const InputClassName = 'mb-8 border-2 border-zinc-800/30 text-center py-3 w-3/4 rounded-md text-lg';
+
 export default function LabelInputProfile({ labelText, type, name, required, readOnly, disabled }) {
   return (
     <>
       <label className='font-medium underline select-none' htmlFor={name}>{labelText}</label>
-      <input className='mb-8 border-2 border-zinc-800/30 text-center py-3 w-3/4 rounded-md text-lg' type={type} id={name} required={required} readOnly={readOnly} disabled={disabled} />
+      {type === 'textarea' ? <textarea className={InputClassName + ' resize-y'} type={type} id={name} required={required} readOnly={readOnly} disabled={disabled} /> : <input className={InputClassName} type={type} id={name} required={required} readOnly={readOnly} disabled={disabled} />}
     </>
   )
 }
